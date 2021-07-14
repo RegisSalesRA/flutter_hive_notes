c2d7c739c18dadbd7e1c90930ef3cd5b1feda807
RegisSalesRA <regismsales@gmail.com>
2021-07-14 10:02:27 -0300

    Hive Crud

---
+++ lib/model/todo_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TodoModelAdapter extends TypeAdapter<TodoModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TodoModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TodoModel(
+      title: fields[0] as String,
+      detail: fields[1] as String,
+      isCompleted: fields[2] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TodoModel obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.detail)
+      ..writeByte(2)
+      ..write(obj.isCompleted);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TodoModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
